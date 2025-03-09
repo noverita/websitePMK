3cc87d61467b891788367792a5282e5043c2f76e
putriisabellaa <putriisabella1007@gmail.com>
2025-03-09 10:34:30 +0700

    Fix:datatable in listpersonnel view

--- resources/views/admin/listpersonel.blade.php
+++ resources/views/admin/listpersonel.blade.php
@@ -2,209 +2,117 @@
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <title>List Data Diri Personel</title>
 
-
-    <title>List Data Diri Personel </title>
-
-    <!-- Custom fonts for this template -->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="{{asset("css/dashboard-admin.css")}}" rel="stylesheet">
-
-    <!-- Custom styles for this page -->
-    <link href="https://code.jquery.com/jquery-3.7.1.js" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js" rel="stylesheet">
-    <link href="https://cdn.datatables.net/2.2.2/js/dataTables.js" rel="stylesheet">
-    <link href="https://cdn.datatables.net/2.2.2/js/dataTables.bootstrap4.js" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css" rel="stylesheet">
-    <link href="https://cdn.datatables.net/2.2.2/css/dataTables.bootstrap4.css" rel="stylesheet">
-
-
-
+    <!-- Custom fonts and styles -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,600,700,800,900" rel="stylesheet">
+    <link href="{{asset('css/dashboard-admin.css')}}" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.2.2/css/dataTables.dataTables.css">
 </head>
 
 <body id="page-top">
-
-    <!-- Page Wrapper -->
     <div id="wrapper">
-
         <!-- Sidebar -->
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-            <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
                 <div class="sidebar-brand-text mx-3">FIRE-FIT</div>
             </a>
-
-            <!-- Divider -->
             <hr class="sidebar-divider my-0">
-
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
-                <a class="nav-link" a href="{{route('admin.dashboard')}}">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard Kesiapan Kerja</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" a href="{{route('list.personel')}}">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>List Data Diri Personel</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" a href="{{route('laporan.personel')}}">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Laporan</span></a>
-            </li>
-            {{-- <li><button type="button" class="btn btn-default" aria-label="Log Out">
-                <span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>
-              </button></li> --}}
-
+            <li class="nav-item"><a class="nav-link" href="{{route('admin.dashboard')}}"><i class="fas fa-fw fa-tachometer-alt"></i> Dashboard</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{route('list.personel')}}"><i class="fas fa-fw fa-users"></i> List Data Diri</a></li>
+            <li class="nav-item"><a class="nav-link" href="{{route('laporan.personel')}}"><i class="fas fa-fw fa-file-alt"></i> Laporan</a></li>
         </ul>
         <!-- End of Sidebar -->
 
-        <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
-
-            <!-- Main Content -->
             <div id="content">
-
                 <!-- Topbar -->
-                <nav class="navbar navbar-expand-lg navbar-light bg-white shadow mb-4 static-top">
-
+                <nav class="navbar navbar-expand-lg navbar-light bg-white shadow mb-4">
                     <div class="container-fluid d-flex justify-content-between align-items-center">
-
-                        <!-- Sidebar Toggle (Topbar) -->
-                        <button id="sidebarToggleTop" class="btn btn-link d-lg-none rounded-circle mr-3">
-                            <i class="fa fa-bars"></i>
-                        </button>
-
-                        <!-- Topbar Search (Hidden di layar kecil) -->
+                        <button id="sidebarToggleTop" class="btn btn-link d-lg-none rounded-circle"><i class="fa fa-bars"></i></button>
                         <form class="d-none d-md-flex form-inline flex-grow-1 mx-3">
                             <div class="input-group w-100">
-                                <input type="text" class="form-control bg-light border-0 small"
-                                    placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                                <input type="text" class="form-control bg-light border-0 small" placeholder="Search...">
                                 <div class="input-group-append">
-                                    <button class="btn btn-primary" type="button">
-                                        <i class="fas fa-search fa-sm"></i>
-                                    </button>
+                                    <button class="btn btn-primary" type="button"><i class="fas fa-search fa-sm"></i></button>
                                 </div>
                             </div>
                         </form>
-
-                        <!-- Logout Button -->
-                        <form method="POST" action="{{ route('logout') }}" class="d-flex align-items-center">
+                        <form method="POST" action="{{ route('logout') }}">
                             @csrf
                             <button type="submit" class="btn btn-danger btn-sm">Logout</button>
                         </form>
                     </div>
-
                 </nav>
-
                 <!-- End of Topbar -->
 
-                <!-- Begin Page Content -->
                 <div class="container-fluid">
-
-                    <!-- Page Heading -->
                     <h2 class="text-center font-weight-bold">List Data Diri Personel</h2>
-
-                    <a href="{{ route('data.personel') }}" class="btn btn-primary">+ Create</a>
-
-                    <hr>
-                    <!-- DataTales Example -->
-                    <script>$('#example').DataTable();</script>
-                    <table id="example" class="table table-striped table-bordered" style="width:100%">
-                        <thead>
-                            <tr>
-                                <th>Nama Lengkap</th>
-                                <th>NIK</th>
-                                <th>Role</th>
-                                <th>Grade</th>
-                                <th>Opsi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td>
-                                    <button type="button" class="btn btn-warning">Edit</button>
-                                    <button type="button" class="btn btn-danger">Hapus</button>
-                                </td>
-                            </tr>
-
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <th>Nama Lengkap</th>
-                                <th>NIK</th>
-                                <th>Role</th>
-                                <th>Grade</th>
-                                <th>Opsi</th>
-                            </tr>
-                        </tfoot>
-                    </table>
-                <!-- /.container-fluid -->
-
+                    <div class="card">
+                        <div class="card-body">
+                            <a href="{{ route('data.personel') }}" class="btn btn-primary mb-2 w-20">+ Create</a>
+                            <table id="example" class="table table-striped" style="width:100%">
+                                <thead>
+                                    <tr>
+                                        <th>Nama</th>
+                                        <th>NIK</th>
+                                        <th>Role</th>
+                                        <th>Grade</th>
+                                        <th>Opsi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Donna Snider</td>
+                                        <td>0000000000001</td>
+                                        <td>Personnel</td>
+                                        <td>-</td>
+                                        <td>
+                                            <a href="http://" class="btn btn-warning btn-sm">Edit</a>
+                                            <a href="http://" class="btn btn-danger btn-sm">Hapus</a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Noverita</td>
+                                        <td>0000000000001</td>
+                                        <td>Personnel</td>
+                                        <td>-</td>
+                                        <td>
+                                            <a href="http://" class="btn btn-warning btn-sm">Edit</a>
+                                            <a href="http://" class="btn btn-danger btn-sm">Hapus</a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    
+                </div>
             </div>
-            <!-- End of Main Content -->
-
             <!-- Footer -->
             <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; PMK 2025</span>
-                    </div>
+                <div class="container my-auto text-center">
+                    <span>Copyright &copy; PMK 2025</span>
                 </div>
             </footer>
-            <!-- End of Footer -->
-
         </div>
-        <!-- End of Content Wrapper -->
-
     </div>
-    <!-- End of Page Wrapper -->
 
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
-
-
-
-    {{-- <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
-
-    <!-- Page level plugins -->
-    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
-
-    <!-- Page level custom scripts -->
-    <script src="js/demo/datatables-demo.js"></script> --}}
-
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdn.datatables.net/2.2.2/js/dataTables.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable();
+        });
+    </script>
 </body>
 
 </html>
